600a79afb80272f7578554206233d9e005543538
Eliot <fadookie@gmail.com>
2015-08-25 07:13:15 -0700

    Ignore -p flag if passed to script by Platypus/Carbon. See https://github.com/sveinbjornt/Platypus/issues/55

--- src/SecureDelete.bash
+++ src/SecureDelete.bash
@@ -1,3 +1,20 @@
 #!/bin/bash
-#shift #removes first argument which is process ID
+
+# Parse out unwanted -p flag that may get sent by Platypus/Carbon
+while getopts "p::" Option
+do
+  case $Option in
+    p     ) echo "ignoring option -p-\
+                  with argument \"$OPTARG\"   [OPTIND=${OPTIND}]";;
+    #  Note that option 'p' must have an associated argument,
+    #+ otherwise it falls through to the default.
+    *     ) echo "Unimplemented option chosen.";;   # Default.
+  esac
+done
+
+#  Decrements the argument pointer so it points to next argument.
+#  $1 now references the first non-option item supplied on the command-line
+#+ if one exists.
+shift $(($OPTIND - 1))
+
 srm -R "$@"
